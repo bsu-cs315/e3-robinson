f49afed8e863794980c7bceb9bcba53f4e76ece5
williamrobinsonbsu <william.robinson@bsu.edu>
2024-10-09 20:15:41 -0400

    Add projectile firing and targets

--- project/cannon/cannon.gd
+++ project/cannon/cannon.gd
@@ -1,18 +1,18 @@
 extends Node3D
 
 
-signal launched_ball(ball: Ball)
 @export var launch_angle := -TAU/8
 @export var angle_change_rate := TAU/8
-@export var power := 800.0
-@export var power_change_rate := 100
+@export var power := 50.0
+@export var power_change_rate := 10
 @export var swivel := -TAU/8
 @export var swivel_change_rate := TAU/8
 var _launched := false
 
 @onready var cannon_shaft: CSGCylinder3D = $CSGCylinder3D
-@onready var angle_label: Label = $Camera3D/AngleLabel
-@onready var power_label: Label = $Camera3D/PowerLabel
+@onready var angle_label: Label = $AngleLabel
+@onready var power_label: Label = $PowerLabel
+@onready var spawn_point: Marker3D = $CSGCylinder3D/SpawnPoint
 
 
 func _physics_process(delta: float) -> void:
@@ -26,7 +26,7 @@ func _physics_process(delta: float) -> void:
 	var power_axis := Input.get_axis("power_down", "power_up")
 	power += power_axis * power_change_rate * delta
 	
-	power = clampf(power, 0, 100)
+	power = clampf(power, 0, 50)
 	
 	var swivel_axis := Input.get_axis("turn_right", "turn_left")
 	swivel += swivel_axis * swivel_change_rate * delta
@@ -34,3 +34,14 @@ func _physics_process(delta: float) -> void:
 	
 	angle_label.text = "Angle: %.d" % rad_to_deg(launch_angle)
 	power_label.text = "Power: %.d" % power
+	
+	if Input.is_action_just_pressed("launch") and _launched == false:
+		_launched = true
+		var impulse := self.get_global_transform().basis.z
+		var ball: RigidBody3D = preload("res://ball/ball.tscn").instantiate()
+		get_parent().add_child(ball)
+		ball.global_position = spawn_point.global_position
+		ball.apply_impulse(-1 * impulse * power)
+		await get_tree().create_timer(3).timeout
+		ball.queue_free()
+		_launched = false
---
+++ project/target/target.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+
+var _hit = false
+
+@onready var color: CSGCylinder3D = $CSGCylinder3D
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group("balls") and _hit == false:
+		color.material.albedo_color = Color.GREEN
+		_hit = true
+		
