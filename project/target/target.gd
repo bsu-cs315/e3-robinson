3aa8a5047b639eb1a6d98a7409396754a6c3735c
williamrobinsonbsu <william.robinson@bsu.edu>
2024-10-09 20:20:13 -0400

    Add score tracking

--- project/target/target.gd
+++ project/target/target.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 
+signal hit
 var _hit = false
 
 @onready var color: CSGCylinder3D = $CSGCylinder3D
@@ -10,4 +11,4 @@ func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("balls") and _hit == false:
 		color.material.albedo_color = Color.GREEN
 		_hit = true
-		
+		hit.emit()
--- project/world.gd
+++ project/world.gd
@@ -7,3 +7,7 @@ extends Node3D
 
 func _on_cannon_launched() -> void:
 	times_fired += 1
+
+
+func _on_target_hit() -> void:
+	score += 1
